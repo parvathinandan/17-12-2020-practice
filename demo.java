943fd00670f2eba2f89fbca83eb83087dbf22e33
parvathinandan <61867180+parvathinandan@users.noreply.github.com>
2020-11-24 16:48:17 +0530

    add method added in demo class

--- demo.java
+++ demo.java
@@ -3,6 +3,9 @@ class Main {
 	Integer id;
 	String name;
 
-	static {System.out.println("Hiiiii , how are you ?") }
-
+	static {System.out.println("Hiiiii , how are you ?") };
+	
+	public void add() {
+	   sysout("hi , i am neo"); }
+ 
 }
